b1b92cde05edab5746bd91bbf21c59d5a4bccf05
Anshuman56 <anshumandehury@yandex.com>
2025-06-24 00:27:34 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)81.js
@@ -0,0 +1,26 @@
+/*In English, all words at the begining of a sentence should begin with a capital letter.
+
+You will be given a paragraph that does not use capital letters. Your job is to capitalise the first letter of the first word of each sentence.
+
+For example,
+
+input:
+
+"hello. my name is inigo montoya. you killed my father. prepare to die."
+
+output:
+
+"Hello. My name is inigo montoya. You killed my father. Prepare to die."
+
+You may assume:
+
+there will be no punctuation besides full stops and spaces
+
+all but the last full stop will be followed by a space and at least one word*/
+
+
+function fix(paragraph){
+  if(paragraph === '')
+    return ''
+  return paragraph.split('. ').map(item => item.split('').map((data, index) => index === 0?data.toUpperCase():data).join('')).join('. ')
+}
