e7c3a6d7838ae1880b867749528dc0f26a88b1b0
Anshuman56 <anshumandehury@yandex.com>
2025-07-05 05:24:42 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)106.js
@@ -0,0 +1,22 @@
+/*In this kata you must take an input string, reverse the order of the words, and reverse the order of the letters within the words.
+
+But, as a bonus, every test input will end with a punctuation mark (! ? .) and the output should be returned with the mark at the end.
+
+A few examples should help clarify:
+
+esrever("hello world.") == "dlrow olleh."
+
+esrever("Much l33t?") == "t33l hcuM?"
+
+esrever("tacocat!") == "tacocat!"
+Quick Note: A string will always be passed in (though it may be empty) so no need for error-checking other types.*/
+
+
+function esrever( st ){
+  // ...
+  if(st === '')
+    return ''
+  let last = st.split('').pop();
+  let str = st.slice(0, st.length -1)
+  return str.split(' ').map(item => item.split('').reverse().join('')).reverse().join(' ')+last;
+}
