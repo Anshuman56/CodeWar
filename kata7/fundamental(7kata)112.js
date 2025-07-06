824374cb6a3c73a2a03bbafc07879295cd4b9a7b
Anshuman56 <anshumandehury@yandex.com>
2025-07-06 06:43:46 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)112.js
@@ -0,0 +1,24 @@
+/*Being a bald man myself, I know the feeling of needing to keep it clean shaven. Nothing worse that a stray hair waving in the wind.
+
+You will be given a string(x). Clean shaved head is shown as "-" and stray hairs are shown as "/". Your task is to check the head for stray hairs and get rid of them.
+
+You should return the original string, but with any stray hairs removed. Keep count ot them though, as there is a second element you need to return:
+
+0 hairs --> "Clean!"
+1 hair --> "Unicorn!"
+2 hairs --> "Homer!"
+3-5 hairs --> "Careless!"
+>5 hairs --> "Hobo!"
+
+So for this head: "------/------" you shoud return:
+
+["-------------", "Unicorn"]*/
+
+
+function bald(x){
+  if(!x.includes('/'))
+    return [x, 'Clean!']
+  let arr = x.split('/')
+  let s = x.split('').map(item => item === '/'?'-':item).join('')
+return arr.length === 2? [s, 'Unicorn!']:arr.length === 3?[s, "Homer!"]:arr.length >= 4 && arr.length <= 6?[s, "Careless!"]:[s, "Hobo!"];
+}
