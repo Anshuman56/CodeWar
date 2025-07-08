cc06879217996ad0e42f54297d12b80b89ff981d
Anshuman56 <anshumandehury@yandex.com>
2025-07-07 22:46:53 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)116.js
@@ -0,0 +1,20 @@
+/*Write a function that accepts an integer argument n and generates an array containing the pairs of integers [a, b] that satisfy the condition
+
+0 <= a <= b <= n
+The pairs should be sorted by increasing values of a, then by increasing values of b.
+
+For example,
+
+for input: 2
+it should return: [  [0, 0], [0, 1], [0, 2],  [1, 1], [1, 2],  [2, 2]  ]*/
+
+
+function generatePairs(n) {
+    var result = [];
+  for(i =0 ; i <= n ; i++){
+    for(j = i ; j <= n ; j++){
+        result.push([i, j]);
+    }
+  }
+  return result;
+}
