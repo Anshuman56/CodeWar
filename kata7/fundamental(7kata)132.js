85324e4b43d4cd065f0560eb50ba78a75153593b
Anshuman56 <anshumandehury@yandex.com>
2025-07-14 03:50:01 -0700

    add some string fundamental 7th 8th kata

---
+++ kata7/fundamental(7kata)132.js
@@ -0,0 +1,17 @@
+/*Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+
+For example:
+
+add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like 
+this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]*/
+
+
+function add(arr) {
+  let result = [];
+  let sum = 0;
+  for(let i =0; i < arr.length; i++){
+    sum += arr[i];
+    result.push(sum);
+  }
+  return result;
+}
