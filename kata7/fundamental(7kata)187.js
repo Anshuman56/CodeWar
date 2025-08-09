ed00f01465e902c8548abab3c9aa40411a883704
Anshuman56 <anshumandehury@yandex.com>
2025-08-09 05:36:57 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)187.js
@@ -0,0 +1,21 @@
+/*Define the toBits(minLength) function converting provided number to a sequence of bits. The default minimum length should be 8.
+
+Examples
+(0).toBits() === '00000000';
+(7).toBits() === '00000111';
+(128).toBits() === '10000000';
+(255).toBits() === '11111111';
+
+(0).toBits(0) === '0';
+(7).toBits(2) === '111';
+(128).toBits(4) === '10000000';
+(255).toBits(4) === '11111111';*/
+
+
+Number.prototype.toBits = function(length=8) {
+  let s = this.toString(2).split('');
+  for(let i =s.length; i < length; i++){
+    s.unshift('0')
+  }
+  return s.join('')
+}
