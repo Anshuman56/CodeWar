869aafce9c62ad7402b03ee8bb209f213c6887fc
Anshuman56 <anshumandehury@yandex.com>
2025-08-22 02:53:58 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)231.js
@@ -0,0 +1,10 @@
+/*Please write a function that sums a list, but ignores any duplicated items in the list.
+
+For instance, for the list [3, 4, 3, 6] the function should return 10,
+and for the list [1, 10, 3, 10, 10] the function should return 4.*/
+
+
+function sumNoDuplicates(numList) {
+  let duplicate = numList.filter((item, index) => numList.indexOf(item) !== index);
+  return numList.filter(item => !duplicate.includes(item)).reduce((acc, item) => acc + item, 0);
+}
