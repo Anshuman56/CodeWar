5cb88c2e5b10ffd9dd8bc8939ae75cc031890421
Anshuman56 <anshumandehury@yandex.com>
2025-08-29 07:05:18 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)247.js
@@ -0,0 +1,16 @@
+/*Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"*/
+
+
+
+function longest(s1, s2) {
+  // your code
+  return s1.concat(s2).split('').filter((item, index, self) => self.indexOf(item) === index).sort().join('')
+}
