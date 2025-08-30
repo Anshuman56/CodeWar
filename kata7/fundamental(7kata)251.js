517a31b23286cc9d96dd80c52736a1205756cefe
Anshuman56 <anshumandehury@yandex.com>
2025-08-30 07:04:31 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)251.js
@@ -0,0 +1,10 @@
+/*Write a function that adds a named property to an object. It must be possible to set the property to a new value. If the property already exists on the object, and error should be thrown.*/
+
+
+function addProperty(obj, prop, value) {
+  // Add your code here
+  if (prop in obj) {
+        throw new Error(`error`);
+    }
+  return obj[prop] = value;
+}
