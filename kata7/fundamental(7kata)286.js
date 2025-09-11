34370b2a3d1c97d17ccad54b1016ef286c5c54fb
Anshuman56 <anshumandehury@yandex.com>
2025-09-10 22:15:06 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)286.js
@@ -0,0 +1,35 @@
+/*Find and return second largest unique number in given Array.
+
+Array may contain values of any data type. Ignore everything except numbers and strings, which may be converted to numbers.
+
+If passed value is not an Array or if there is no second largest item in given Array (e.g. [1, 1, 1]) should return undefined.
+
+Examples
+secondLargest([1, -2, 3]) = 1;
+secondLargest([5, 5, 4]) = 4;
+secondLargest([2, '3', 4]) = 3;
+Should return
+// 1. Passed value not an Array:
+secondLargest("1 2 3") = undefined;
+
+// 2. There is no second largest:
+secondLargest([2, 2, 2]) = undefined;
+
+// 3. Ignore everything except:
+//   * numbers
+//   * not-NaN strings:
+secondLargest(['-1', 2, null, false]) = -1;
+Input range
+Numbers are safe Integers.
+Not-NaN strings represent safe Integers.*/
+
+
+
+function secondLargest(array){
+  // Have Fun ^_^
+  if(!Array.isArray(array) || array.filter(item => array.indexOf(item) === array.lastIndexOf(item)).length === 0)
+    return undefined;
+  
+  let l = array.filter(item => typeof item === 'number'|| typeof item === 'string').map(item => +item).filter(item => !Number.isNaN(item)).sort((a, b) => a - b);
+  return l[l.length - 2]
+}
