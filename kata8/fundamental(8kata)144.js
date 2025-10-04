43efdf3760e15ab00d4d15004e04b690793914f8
Anshuman56 <anshumandehury@yandex.com>
2025-10-04 05:24:04 -0700

    add some string fundamental 8th kata

---
+++ kata8/fundamental(8kata)144.js
@@ -0,0 +1,12 @@
+/*function toFreud(string) {
+  if(string === '')
+    return '';
+return string.split(' ').map(item =>'sex').join(' ');
+}*/
+
+
+function toFreud(string) {
+  if(string === '')
+    return '';
+return string.split(' ').map(item =>'sex').join(' ');
+}
