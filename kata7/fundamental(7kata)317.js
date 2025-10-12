d2d351708599b8b9330ad545adb470de9c98a3c0
Anshuman56 <anshumandehury@yandex.com>
2025-10-12 05:43:04 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)317.js
@@ -0,0 +1,9 @@
+/*Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.*/
+
+
+
+String.prototype.isLetter = function() {
+
+return /^[a-zA-Z]$/i.test(this)
+  
+}
