d37cea35ba5368eda16a354aa81407e2bc2d6717
Anshuman56 <anshumandehury@yandex.com>
2025-10-17 06:06:16 -0700

    add some string fundamental 7th kata

---
+++ kata7/fundamental(7kata)329.js
@@ -0,0 +1,17 @@
+/*Write a function named numbers.
+
+function should return True if all parameters are of the Number type.
+
+The function should accept any number of parameters.
+
+Example usage:
+
+numbers(1, 4, 3, 2, 5); // true
+numbers(1, "a", 3); // false
+numbers(1, 3, NaN); // true*/
+
+
+// Create your numbers function here
+function numbers(...nums){
+  return nums.filter(item => typeof item !== 'number').length > 0 ? false : true
+}
