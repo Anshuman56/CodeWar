952e044d7077ab55c7754fa95586820713292912
Anshuman56 <anshumandehury@yandex.com>
2025-11-10 04:57:58 -0800

    add fundamental 7 kata

---
+++ kata7/fundamental(7kata)366.js
@@ -0,0 +1,16 @@
+/*Given a string, return true if the first instance of "x" in the string is immediately followed by the string "xx".
+
+"abraxxxas" → true
+"xoxotrololololololoxxx" → false
+"softX kitty, warm kitty, xxxxx" → true
+"softx kitty, warm kitty, xxxxx" → false
+Note :
+
+capital X's do not count as an occurrence of "x".
+if there are no "x"'s then return false*/
+
+
+function tripleX(str){
+  let first = str.indexOf('x');
+  return str.slice(first, first + 3) === 'xxx'
+}
